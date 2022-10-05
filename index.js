0d39cd937700ad0432c3d708f324ff30d7c66d11
monzuralam16 <monzur.alam.16@gmail.com>
2022-10-05 15:36:42 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,29 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json()); // this line let us parse the req.body
 
+// JWT 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    // if token not available
+    if (!authHeader) {
+        return res.status(401).send({
+            message: "Unauthorized Access"
+        })
+    }
+    const token = authHeader.split(' ')[1];
+    //check token as verified
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({
+                message: "invalid token"
+            })
+        }
+        req.decoded = decoded;
+        console.log("decoded", decoded);
+        next();
+    })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gg0k3o8.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,12 +45,43 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('genius-car').collection('service');
+        const orderCollection = client.db('genius-car').collection('order');
+
+        //Token create
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken })
+        })
+
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        });
+
+
+
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.user.emailValue;
+            const email = req.query.email;
+
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders)
+            }
+            else {
+                res.status(403).send({
+                    message: 'email not valid'
+                })
+            }
         });
 
         app.get('/service/:id', async (req, res) => {
@@ -40,6 +95,13 @@ async function run() {
         app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        // post order data 
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
